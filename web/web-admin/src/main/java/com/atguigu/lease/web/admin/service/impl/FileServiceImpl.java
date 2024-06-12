bcf8f0409b6854c38aab3e1f629a1504f830027e
GotohHitori <hitori150221@outlook.com>
2024-06-12 20:35:41 +0800

    全局异常处理器实现

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
@@ -25,38 +25,34 @@ public class FileServiceImpl implements FileService {
 
 
     @Override
-    public String upload(MultipartFile file) {
-        try {
-            boolean bucketExists = minioClient.
-                    bucketExists(BucketExistsArgs.builder().
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        boolean bucketExists = minioClient.
+                bucketExists(BucketExistsArgs.builder().
+                        bucket(minioProperties.getBucketName()).build());
+        // 如果没有这个存储桶就创建
+        if (!bucketExists) {
+            minioClient.
+                    makeBucket(MakeBucketArgs.builder().
                             bucket(minioProperties.getBucketName()).build());
-            // 如果没有这个存储桶就创建
-            if (!bucketExists) {
-                minioClient.
-                        makeBucket(MakeBucketArgs.builder().
-                                bucket(minioProperties.getBucketName()).build());
-                // 设置存储桶策略
-                minioClient.
-                        setBucketPolicy(SetBucketPolicyArgs.builder().
-                                bucket(minioProperties.getBucketName()).
-                                config(createBucketPolicyConfig(minioProperties.getBucketName())).
-                                build());
-            }
-
-            String fileName = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" +
-                    UUID.randomUUID() + "-" + file.getOriginalFilename();
-            minioClient.putObject(PutObjectArgs.builder()
-                    .bucket(minioProperties.getBucketName())
-                    .object(fileName)
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .contentType(file.getContentType())
-                    .build());
-
-            return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), fileName);
-        } catch (Exception e) {
-            e.printStackTrace();
+            // 设置存储桶策略
+            minioClient.
+                    setBucketPolicy(SetBucketPolicyArgs.builder().
+                            bucket(minioProperties.getBucketName()).
+                            config(createBucketPolicyConfig(minioProperties.getBucketName())).
+                            build());
         }
-        return null;
+
+        String fileName = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" +
+                UUID.randomUUID() + "-" + file.getOriginalFilename();
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .object(fileName)
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .contentType(file.getContentType())
+                .build());
+
+        return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), fileName);
+
     }
 
     /**
