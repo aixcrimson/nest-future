9aca949b3d227e66783b646c48fbd73a30e134fc
GotohHitori <hitori150221@outlook.com>
2024-06-13 16:18:05 +0800

    房间管理业务开发

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/RoomController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/RoomController.java
@@ -4,6 +4,7 @@ package com.atguigu.lease.web.admin.controller.apartment;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.RoomInfo;
 import com.atguigu.lease.model.enums.ReleaseStatus;
+import com.atguigu.lease.web.admin.service.RoomInfoService;
 import com.atguigu.lease.web.admin.vo.room.RoomDetailVo;
 import com.atguigu.lease.web.admin.vo.room.RoomItemVo;
 import com.atguigu.lease.web.admin.vo.room.RoomQueryVo;
@@ -11,6 +12,7 @@ import com.atguigu.lease.web.admin.vo.room.RoomSubmitVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,40 +22,55 @@ import java.util.List;
 @RequestMapping("/admin/room")
 public class RoomController {
 
+    @Autowired
+    private RoomInfoService roomInfoService;
+
     @Operation(summary = "保存或更新房间信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody RoomSubmitVo roomSubmitVo) {
+        roomInfoService.saveOrUpdateRoom(roomSubmitVo);
         return Result.ok();
     }
 
     @Operation(summary = "根据条件分页查询房间列表")
     @GetMapping("pageItem")
     public Result<IPage<RoomItemVo>> pageItem(@RequestParam long current, @RequestParam long size, RoomQueryVo queryVo) {
-        return Result.ok();
+        IPage<RoomItemVo> page = roomInfoService.pageItem(current, size, queryVo);
+        return Result.ok(page);
     }
 
     @Operation(summary = "根据id获取房间详细信息")
     @GetMapping("getDetailById")
     public Result<RoomDetailVo> getDetailById(@RequestParam Long id) {
-        return Result.ok();
+        RoomDetailVo roomDetailVo = roomInfoService.getDetailById(id);
+        return Result.ok(roomDetailVo);
     }
 
     @Operation(summary = "根据id删除房间信息")
     @DeleteMapping("removeById")
     public Result removeById(@RequestParam Long id) {
+        roomInfoService.removeRoomById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据id修改房间发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(Long id, ReleaseStatus status) {
+        roomInfoService.lambdaUpdate()
+                .eq(id != null,RoomInfo::getId, id)
+                .set(RoomInfo::getIsRelease, status)
+                .update();
         return Result.ok();
     }
 
     @GetMapping("listBasicByApartmentId")
     @Operation(summary = "根据公寓id查询房间列表")
     public Result<List<RoomInfo>> listBasicByApartmentId(Long id) {
-        return Result.ok();
+        List<RoomInfo> roomInfos = roomInfoService.lambdaQuery()
+                .eq(id != null, RoomInfo::getApartmentId, id)
+                .orderByDesc(RoomInfo::getCreateTime)
+                .list();
+        return Result.ok(roomInfos);
     }
 
 }
