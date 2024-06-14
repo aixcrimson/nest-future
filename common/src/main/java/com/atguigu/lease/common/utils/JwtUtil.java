4185e74be0bf744d28b15492174ccea25fc6d468
GotohHitori <hitori150221@outlook.com>
2024-06-14 18:15:38 +0800

    登录拦截器实现

--- common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
+++ common/src/main/java/com/atguigu/lease/common/utils/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.atguigu.lease.common.utils;
 
-import io.jsonwebtoken.Jwts;
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
@@ -20,5 +22,24 @@ public class JwtUtil {
                 signWith(tokenSignKey).
                 compact();
         return token;
+    }
+
+    public static Claims parseToken(String token){
+        if (token==null){
+            throw new LeaseException(ResultCodeEnum.ADMIN_LOGIN_AUTH);
+        }
+
+        try{
+            JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(tokenSignKey).build();
+            return jwtParser.parseClaimsJws(token).getBody();
+        }catch (ExpiredJwtException e){
+            throw new LeaseException(ResultCodeEnum.TOKEN_EXPIRED);
+        }catch (JwtException e){
+            throw new LeaseException(ResultCodeEnum.TOKEN_INVALID);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(createToken(2L, "user"));
     }
 }
