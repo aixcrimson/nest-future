9c103dce841e6abbd4605fa78f2e4add485e986f
GotohHitori <hitori150221@outlook.com>
2024-06-15 21:33:31 +0800

    根据公寓id分页查询房间列表

--- web/web-app/src/main/java/com/atguigu/lease/web/app/mapper/RoomInfoMapper.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/mapper/RoomInfoMapper.java
@@ -20,4 +20,6 @@ public interface RoomInfoMapper extends BaseMapper<RoomInfo> {
     IPage<RoomItemVo> pageRoomItemByQuery(IPage<RoomItemVo> page, RoomQueryVo queryVo);
 
     BigDecimal selectMinRentByApartmentId(Long id);
+
+    IPage<RoomItemVo> pageItemByApartmentId(IPage<RoomItemVo> page, Long id);
 }
