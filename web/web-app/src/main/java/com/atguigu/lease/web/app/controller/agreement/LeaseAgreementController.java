29a26c3d80edf0ac8db3b0b7a120ae1fd30b8e4b
GotohHitori <hitori150221@outlook.com>
2024-06-15 22:34:52 +0800

    根据id获取租约详细信息和根据id更新租约状态

--- web/web-app/src/main/java/com/atguigu/lease/web/app/controller/agreement/LeaseAgreementController.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/controller/agreement/LeaseAgreementController.java
@@ -39,12 +39,17 @@ public class LeaseAgreementController {
     @Operation(summary = "根据id更新租约状态", description = "用于确认租约和提前退租")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam LeaseStatus leaseStatus) {
+        leaseAgreementService.lambdaUpdate()
+                .eq(LeaseAgreement::getId, id)
+                .set(LeaseAgreement::getStatus, leaseStatus)
+                .update();
         return Result.ok();
     }
 
     @Operation(summary = "保存或更新租约", description = "用于续约")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody LeaseAgreement leaseAgreement) {
+        leaseAgreementService.saveOrUpdate(leaseAgreement);
         return Result.ok();
     }
 
